2d7ef6b99d50d587cacdfe022907dd75c9a1b150
mhmdnurf <mhmd.nurfat@gmail.com>
2023-05-27 03:29:57 +0700

    implement mysql on fastapi

--- main.py
+++ main.py
@@ -1,24 +1,33 @@
-from typing import Union
-from fastapi import FastAPI
-from pydantic import BaseModel
+from fastapi import FastAPI, HTTPException
+from sqlalchemy.orm import sessionmaker
+from models import Base, User
+from sqlalchemy.exc import IntegrityError
+from sqlalchemy import create_engine
 
+db_url = "mysql+mysqlconnector://root:@localhost/fastapi-test"
+engine = create_engine(db_url)
 
 app = FastAPI()
+SessionLocal = sessionmaker(bind=engine)
+Base.metadata.create_all(bind=engine)
 
-class Item(BaseModel):
-    name: str
-    price: float
-    is_offer: Union[bool, None] = None
+@app.post("/register")
+def register(username: str, password: str):
+    session = SessionLocal()
+    user = User(username=username, password=password)
+    session.add(user)
+    try:
+        session.commit()
+        return {"message": "User registered successfully"}
+    except IntegrityError:
+        raise HTTPException(status_code=400, detail="Username already exists")
+    finally:
+        session.close()
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
-
-@app.put("/items/{item_id}")
-def update_item(item_id: int, item: Item):
-    return {"item_name": item.name, "item_id": item_id}
+@app.post("/login")
+def login(username: str, password: str):
+    session = SessionLocal()
+    user = session.query(User).filter_by(username=username).first()
+    if user and user.password == password:
+        return {"message": "Login successful"}
+    raise HTTPException(status_code=401, detail="Invalid username or password")
