5ab7cc46dd57e770dca75949f7c8fc891ad8b2b5
mhmdnurf <mhmd.nurfat@gmail.com>
2023-05-29 01:17:22 +0700

    add JWT

--- main.py
+++ main.py
@@ -1,15 +1,29 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, Depends, HTTPException
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
+from jose import JWTError, jwt
+
 from sqlalchemy.orm import sessionmaker
 from models import Base, User
 from sqlalchemy.exc import IntegrityError
 from sqlalchemy import create_engine
 
+app = FastAPI()
 db_url = "mysql+mysqlconnector://root:@localhost/fastapi-test"
 engine = create_engine(db_url)
-
-app = FastAPI()
 SessionLocal = sessionmaker(bind=engine)
 Base.metadata.create_all(bind=engine)
+
+bearer = HTTPBearer()
+SECRET_KEY = "your-secret-key"
+ALGORITHM = "HS256"
+
+@app.get("/")
+def dashboard(credentials: HTTPAuthorizationCredentials = Depends(bearer)):
+    try:
+        payload = jwt.decode(credentials.credentials, SECRET_KEY, algorithms=[ALGORITHM])
+        return {"message": "Anda telah terautentikasi"}
+    except JWTError:
+        raise HTTPException(status_code=401, detail="Autentikasi gagal")
 
 @app.post("/register")
 def register(username: str, password: str):
@@ -29,5 +43,7 @@ def login(username: str, password: str):
     session = SessionLocal()
     user = session.query(User).filter_by(username=username).first()
     if user and user.password == password:
-        return {"message": "Login successful"}
+        # Generate JWT token
+        token = jwt.encode({"username": username}, SECRET_KEY, algorithm=ALGORITHM)
+        return {"message": "Login successful", "token": token}
     raise HTTPException(status_code=401, detail="Invalid username or password")
